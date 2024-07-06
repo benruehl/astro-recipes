f064e0015a73aeeeb17456886964e7436bee802a
Benjamin Rühl <dev.ruehl@gmail.com>
2024-07-07 00:27:07 +0200

    build: setup gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://benruehl.github.io',
+  base: 'astro-recipes',
   integrations: [icon()],
   vite: {
     css: {
